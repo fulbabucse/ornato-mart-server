205aa039519668dae24802e77f00990d04e20cc3
Fulbabu <fulbabucse@gmail.com>
2022-11-05 02:36:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Ornato Mart server Running");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.7ywptfp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.listen(port, () => {
+  console.log(`Ornato server running on port: ${port}`);
+});
