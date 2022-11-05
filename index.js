bb19b41ed37d90e6f7923a425c4497afa7fa1b47
Fulbabu <fulbabucse@gmail.com>
2022-11-05 19:34:43 +0600

    delete specific orders

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +17,50 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const Cart = client.db("ornatoMart").collection("cart");
+
+    app.post("/cart", async (req, res) => {
+      const product = req.body;
+      const result = await Cart.insertOne(product);
+      res.send(result);
+    });
+
+    app.get("/cart", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = Cart.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await Cart.findOne(query);
+      res.send(result);
+    });
+
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await Cart.deleteOne(query);
+      res.send(result);
+      console.log(result);
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => {
+  console.log(err.name, err.message);
 });
 
 app.listen(port, () => {
