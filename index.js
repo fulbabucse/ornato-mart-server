9541b76523fc0af0f546979df97ac1bafd1c1151
Fulbabu <fulbabucse@gmail.com>
2022-11-06 14:48:24 +0600

    jwt token add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 app.use(cors());
@@ -11,6 +12,21 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Ornato Mart server Running");
 });
+
+const verifyJWT = (req, res, next) => {
+  const authToken = req.headers.authorization;
+  if (!authToken) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authToken.split(" ")[1];
+  jwt.verify(token, process.env.JWT_ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.7ywptfp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -24,19 +40,32 @@ const run = async () => {
     const Cart = client.db("ornatoMart").collection("cart");
     const Products = client.db("ornatoMart").collection("products");
 
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_ACCESS_TOKEN, {
+        expiresIn: "2d",
+      });
+      res.send({ token });
+    });
+
     app.post("/cart", async (req, res) => {
       const product = req.body;
       const result = await Cart.insertOne(product);
       res.send(result);
     });
 
-    app.get("/cart", async (req, res) => {
+    app.get("/cart", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ message: "Forbidden access" });
+      }
       let query = {};
       if (req.query.email) {
         query = {
           email: req.query.email,
         };
       }
+
       const cursor = Cart.find(query);
       const result = await cursor.toArray();
       res.send(result);
